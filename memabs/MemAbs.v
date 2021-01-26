dc47a253169ec3d1c67bae666e8d77ce1d544cef
Yu-zh <yu.zhang.yz862@yale.edu>
2021-01-25 19:47:34 -0500

    Poor trick to push the universal quantifier on symbol table to outmost

--- memabs/MemAbs.v
+++ memabs/MemAbs.v
@@ -47,20 +47,17 @@ Inductive li_rel: (query li_d * reply li_d) -> (query li_c * reply li_c) -> Prop
     forall vf sg args ret m,
       li_rel ({|dq_vf:=vf;dq_sg:=sg;dq_args:=args|}, {|dr_retval:=ret|})
              ({|cq_vf:=vf;cq_sg:=sg;cq_args:=args;cq_mem:=m|}, {|cr_retval:=ret;cr_mem:=m|}).
-Local Obligation Tactic := idtac.
+
 Program Definition li_dc: li_d --o li_c :=
   {|
   has '(d, c) := li_rel d c
   |}.
 Next Obligation.
-  intros [[dq1 dr1] [cq1 cr1]] [[dq2 dr2] [cq2 cr2]]. simpl.
-  intros rel1 rel2. intuition.
-  - destruct cq1. destruct cq2. inversion H. clear H. subst.
-    inversion rel1. subst. inversion rel2. subst.
-    f_equal. exploit H1. auto. congruence.
-  - injection H. destruct cr1. destruct cr2. destruct cq1. destruct cq2.
-    inversion rel1. inversion rel2. subst. intros.
-    f_equal. congruence. congruence.
+  destruct d1 as [dq1 dr1]. destruct d2 as [dq2 dr2].
+  destruct c1 as [cq1 cr1]. destruct c2 as [cq2 cr2].
+  intuition.
+  - inv H. inv H0. f_equal. exploit H3. f_equal. inversion 1. auto.
+  - inv H. inv H0. inv H1. auto.
 Defined.
 
 Inductive exec {liA liB: language_interface} (σ: !liA --o !liB): query liB -> list (token liA) -> reply liB -> Prop:=
@@ -73,9 +70,8 @@ Program Definition next {liA liB: language_interface} (σ : !liA --o !liB) w qx 
   has '(s, t) := has σ (w ++ s, (qx, rx)::t);
   |}.
 Next Obligation.
-  intros liA liB sig w q r [s1 t1] [s2 t2] H1 H2 Ht.
-  pose proof (has_coh _ _ _ _ H1 H2). cbn in H. clear H1 H2.
-  edestruct H as [Hr Hl].
+  pose proof (has_coh _ _ _ _ H H0). cbn in H1. clear H H0.
+  intros Ht. edestruct H1 as [Hr Hl].
   apply (@app_coh liA). apply Ht.
   split. inversion Hr. subst. apply H5. auto.
   intro. exploit Hl. f_equal. auto. apply app_inv_head.
@@ -100,9 +96,14 @@ Module Abs.
     | step_at_external: forall s s' qa ra qb rb σ w,
         Smallstep.at_external (C se) s qb ->
         Smallstep.after_external (C se) s rb s' ->
-        (* (w, r) may not be the "right" execution. In other words, (next σ w q r) might be empty.
-           But that's fine because it's still related to s per correctness *)
+        (* (w, r) may not be the "right" execution. In other words, [next σ w q
+           r] might be empty.  But that's fine because it's still related to s
+           per correctness *)
         exec σ qa w ra ->
+        (* drop the memory and make a query. take the reply and attach the same
+        memory to it *)
+        (* this can be further refined by giving the reply a new memory as long
+        as it's larger than the old one *)
         li_rel (qa, ra) (qb, rb) ->
         step (st s σ) E0 (ext s' w (next σ w qa ra))
     | step_after_external: forall s σ,
@@ -240,6 +241,196 @@ Definition get_mem (s: state) : mem :=
   | Returnstate _ _ m => m
   end.
 
+Section SepcCKLR.
+  (* A CKLR dedicated to Clight programs *)
+  Variable p: Clight.program.
+  Variable se: Genv.symtbl.
+  Let vars: block -> Z -> Prop := module_var (globalenv se p).
+  (* the target memory and local variables that belong to the component
+     constitute the klr index *)
+  Inductive sepc_world :=
+    sepcw (m: mem) (Hvb: forall b ofs, vars b ofs -> Mem.valid_block m b).
+  Inductive sepc_acc: relation sepc_world :=
+    acc_intro m1 m2 Hm1 Hm2:
+      (* External call into the component only touches its own variables  *)
+      Mem.unchanged_on (fun b ofs => ~ vars b ofs) m1 m2 ->
+      sepc_acc (sepcw m1 Hm1) (sepcw m2 Hm2).
+
+  Inductive sepc_mm: sepc_world -> mem -> mem -> Prop :=
+    match_intro: forall m m1 m2 Hvb,
+      (* source memory extends into target memory *)
+      Mem.extends m1 m2 ->
+      (* local variables of the component are only modified during external
+         calls so they don't change in the course of internal steps*)
+      Mem.unchanged_on vars m m2 ->
+      (* m1 and locals don't have blocks in common *)
+      (forall b ofs, vars b ofs -> ~ Mem.perm m1 b ofs Max Nonempty) ->
+      sepc_mm (sepcw m Hvb) m1 m2.
+
+  Instance sepc_acc_preo:
+    PreOrder sepc_acc.
+  Proof.
+    split.
+    - intros [m]. constructor.
+      apply Mem.unchanged_on_refl.
+    - intros [m1] [m2] [m3].
+      inversion 1. subst.
+      inversion 1. subst.
+      constructor.
+      eapply Mem.unchanged_on_trans; eauto.
+  Qed.
+
+  Program Definition sepc: cklr :=
+    {|
+    world := sepc_world;
+    wacc := sepc_acc;
+    mi w := inject_id;
+    match_mem w := sepc_mm w;
+    (* a cheap workaround *)
+    match_stbls w se1 se2 := se1 = se2 /\ se = se1;
+    |}.
+
+  (* mi_acc *)
+  Next Obligation.
+    repeat rstep. apply inject_incr_refl.
+  Qed.
+  (* match_stbls_acc *)
+  Next Obligation.
+    rauto.
+  Qed.
+  (* match_stbls_proj *)
+  Next Obligation.
+    intros se1 se2 [<- <-]. apply Genv.match_stbls_id.
+  Qed.
+  (* match_stbls_nextblock *)
+  Next Obligation.
+    erewrite <- Mem.mext_next; eauto.
+    inv H0. auto.
+  Qed.
+  (* cklr_alloc *)
+  Next Obligation.
+    intros [ ] m1 m2 Hm lo hi. inv Hm.
+    destruct (Mem.alloc m1 lo hi) as [m1' b1] eqn: Hm1.
+    edestruct Mem.alloc_extends as (m2' & Hm2' & Hm'); eauto; try reflexivity.
+    rewrite Hm2'.
+    eexists (sepcw m _); split; repeat rstep.
+    constructor; auto.
+    - eapply Mem.unchanged_on_trans; eauto.
+      eapply Mem.alloc_unchanged_on; eauto.
+    - intros. specialize (Hvb _ _ H).
+      specialize (H2 _ _ H). intros Hp. apply H2.
+      eapply Mem.perm_alloc_4 in Hp; eauto.
+      eapply Mem.alloc_result in Hm1. subst.
+      exploit Mem.valid_block_unchanged_on; eauto.
+      erewrite Mem.mext_next; eauto.
+  Qed.
+  (* cklr_free *)
+  Next Obligation.
+    intros [ ] m1 m2 Hm [[b lo] hi] r2 Hr. inv Hm.
+    apply coreflexivity in Hr. subst. simpl. red.
+    destruct (Mem.free m1 b lo hi) as [m1'|] eqn:Hm1'; [|constructor].
+    edestruct Mem.free_parallel_extends as (m2' & Hm2' & Hm'); eauto.
+    rewrite Hm2'. constructor.
+    eexists (sepcw m _); split; repeat rstep.
+    constructor; auto.
+    - eapply Mem.unchanged_on_trans; eauto.
+      eapply Mem.free_unchanged_on; eauto.
+      intros ofs Hofs Hv. specialize (H2 _ _ Hv). apply H2.
+      exploit Mem.free_range_perm. apply Hm1'. eauto.
+      intros Hp. eapply Mem.perm_cur_max.
+      eapply Mem.perm_implies. apply Hp. constructor.
+    - intros. specialize (H2 _ _ H).
+      intros Hp. apply H2.
+      eapply Mem.perm_free_3; eauto.
+  Qed.
+  Require Import Extends.
+  (* cklr_load *)
+  Next Obligation.
+    intros [ ] chunk m1 m2 Hm [b ofs] p2 Hp. inv Hm.
+    apply coreflexivity in Hp; subst. simpl. red.
+    destruct (Mem.load chunk m1 b ofs) as [v1|] eqn:Hv1; [|constructor].
+    edestruct Mem.load_extends as (v2 & Hv2 & Hv); eauto.
+    rewrite Hv2. rewrite val_inject_lessdef_eqrel. rauto.
+  Qed.
+  (* cklr_store *)
+  Next Obligation.
+    intros [ ] chunk m1 m2 Hm [b ofs] p2 Hp v1 v2 Hv. inv Hm.
+    apply coreflexivity in Hp; subst. simpl. red.
+    destruct (Mem.store chunk m1 b ofs v1) as [m1'|] eqn:Hm1'; [|constructor].
+    apply val_inject_lessdef in Hv.
+    edestruct Mem.store_within_extends as (m2' & Hm2' & Hm'); eauto.
+    rewrite Hm2'. constructor.
+    eexists (sepcw m _); split; repeat rstep.
+    constructor; auto.
+    - eapply Mem.unchanged_on_trans; eauto.
+      eapply Mem.store_unchanged_on; eauto.
+      intros ofs' Hofs. intros Hp. specialize (H2 _ _ Hp). apply H2.
+      exploit Mem.store_valid_access_3. apply Hm1'.
+      unfold Mem.valid_access. intros [Hpr ?]. specialize (Hpr _ Hofs).
+      eapply Mem.perm_cur_max. eapply Mem.perm_implies. apply Hpr. constructor.
+    - intros. specialize (H2 _ _ H).
+      intros Hp. apply H2.
+      eapply Mem.perm_store_2; eauto.
+  Qed.
+  (* cklr_loadbytes *)
+  Next Obligation.
+    intros [ ] m1 m2 Hm [b ofs] p2 Hp sz. inv Hm.
+    apply coreflexivity in Hp; subst. simpl. red.
+    destruct (Mem.loadbytes m1 b ofs sz) as [v1|] eqn:Hv1; [|constructor].
+    edestruct Mem.loadbytes_extends as (v2 & Hv2 & Hv); eauto.
+    rewrite Hv2. rauto.
+  Qed.
+  (* cklr_storebytes *)
+  Next Obligation.
+    intros [ ] m1 m2 Hm [b1 ofs1] p2 Hp vs1 vs2 Hv. inv Hm.
+    apply coreflexivity in Hp. subst. simpl. red.
+    destruct (Mem.storebytes m1 b1 ofs1 vs1) as [m1'|] eqn:Hm1'; [|constructor].
+    edestruct Mem.storebytes_within_extends as (m2' & Hm2' & Hm'); eauto.
+    eapply list_rel_forall2. apply Hv.
+    rewrite Hm2'. constructor.
+    eexists (sepcw m _); split; repeat rstep.
+    constructor; auto.
+    - eapply Mem.unchanged_on_trans; eauto.
+      eapply Mem.storebytes_unchanged_on; eauto.
+      intros ofs' Hofs. intros Hp. specialize (H2 _ _ Hp). apply H2.
+      exploit Mem.storebytes_range_perm. apply Hm1'.
+      rewrite length_rel; eauto. intros.
+      eapply Mem.perm_cur_max. eapply Mem.perm_implies; eauto. constructor.
+    - intros. specialize (H2 _ _ H).
+      intros Hp. apply H2.
+      eapply Mem.perm_storebytes_2; eauto.
+  Qed.
+  (* cklr_perm *)
+  Next Obligation.
+    intros [ ] m1 m2 Hm [b1 ofs1] p2 Hp p' k H. inv Hm.
+    apply coreflexivity in Hp. subst. simpl in *.
+    eapply Mem.perm_extends; eauto.
+  Qed.
+  (* cklr_valid_block *)
+  Next Obligation.
+    intros [ ] m1 m2 Hm b1 b2 Hb. inv Hm.
+    apply coreflexivity in Hb. subst.
+    apply Mem.valid_block_extends; eauto.
+  Qed.
+  (* cklr_no_overlap *)
+  Next Obligation.
+    intros b1 b1' delta1 b2 b2' delta2 ofs1 ofs2 Hb Hb1' Hb2' Hp1 Hp2. inv H.
+    inv Hb1'. inv Hb2'. eauto.
+  Qed.
+  (* cklr_representable *)
+  Next Obligation.
+    xomega.
+  Qed.
+  (* cklr_aligned_area_inject *)
+  Next Obligation.
+    rewrite Z.add_0_r. assumption.
+  Qed.
+  (* cklr_disjoint_or_equal_inject *)
+  Next Obligation.
+    intuition xomega.
+  Qed.
+End SepcCKLR.
+
 Section SIM.
   Context {p: Clight.program} {Σ: Genv.symtbl -> !li_d --o !li_d} (ps: prog_sim p Σ).
   Variable C: Clight.program.
@@ -256,20 +447,19 @@ Section SIM.
   Notation " 'state2' " := (@Impl.state li_d li_c (semantics1 C)) (at level 1) : sim_scope.
   Open Scope sim_scope.
 
-  Section MATCH.
-    Inductive state_match {ind} (ms: ind -> state -> state -> Prop): state1 -> state2 -> Prop :=
-    | st_match: forall (s1 s2: state) σ1 i,
-        ms i s1 s2 ->  ps σ1 (get_mem s2)->
-        state_match ms (st1 s1 σ1) (st2 s2)
-    | ext_match: forall (s1 s2: state) σ1 t1 t2 i,
-        ms i s1 s2 -> t1 = t2 ->
-        ps σ1 (get_mem s2)->
-        state_match ms (ext1 s1 t1 σ1) (ext2 s2 t2).
-  End MATCH.
+  Inductive state_match {ind} (ms: ind -> state -> state -> Prop): state1 -> state2 -> Prop :=
+  | st_match: forall (s1 s2: state) σ1 i,
+      ms i s1 s2 ->  ps σ1 (get_mem s2)->
+      state_match ms (st1 s1 σ1) (st2 s2)
+  | ext_match: forall (s1 s2: state) σ1 t1 t2 i,
+      ms i s1 s2 -> t1 = t2 ->
+      ps σ1 (get_mem s2)->
+      state_match ms (ext1 s1 t1 σ1) (ext2 s2 t2).
+
   Variable se: Genv.symtbl.
-  Let vars := module_var (Clight.globalenv se p).
-  Let sepv := sep vars.
-  Instance sepv_frame: KripkeFrame unit (sep_world vars).
+  (* Let vars := module_var (Clight.globalenv se p). *)
+  Let sepv := sepc p se.
+  Instance sepv_frame: KripkeFrame unit (sepc_world p se).
   Proof. exact (cklr_kf sepv). Qed.
 
   Theorem fsim_abs_impl:
@@ -282,18 +472,13 @@ Section SIM.
     eapply Genv.valid_for_linkorder in HseC; eauto.
     eapply Genv.valid_for_linkorder in Hsep; eauto.
     specialize (H se1 se2 w Hse HseC).
-    assert (se = se1). admit. subst se2. subst se1.
-    (* apply forward_simulation_step with *)
-    (*     (match_states := fun s1 s2 => klr_diam tt (state_match (match_st se se)) w s1 s2). *)
+    destruct Hse as [<- <-].
     apply forward_simulation_step with (match_states := (state_match (match_st se se w))).
     - intros. exploit @fsim_match_valid_query; eauto.
-    - cbn. intros q1 q2 s1 Hq Hs1.
-      cbn in Hs1. inv Hs1.
+    - cbn. intros q1 q2 s1 Hq Hs1. cbn in Hs1. inv Hs1.
       edestruct @fsim_match_initial_states as (i & s2 & Hiq & Hs); eauto.
       exists(st2 s2). split. constructor. auto.
       eapply st_match. apply Hs. eapply correct_cond. auto. admit.
-    (* initial query has to satisfy init_mem. Maybe we have to parametrize the
-       semantics over some proposition on memory *)
     - admit.
     - admit.
     - intros s1 t s1' Hstep s2 Hs. inversion Hstep.
@@ -334,7 +519,7 @@ Section SIM.
           }
           rewrite <- H5. apply Hs'.
       + admit.
-    - admit.
+          - admit.
   Admitted.
 
   Close Scope sim_scope.
